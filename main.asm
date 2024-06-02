2c09f37ec01353844260ea950fd45474759ae43e
Timur J <112691067+buerakus@users.noreply.github.com>
2024-06-02 17:18:27 +0800

    Update main.asm

--- main.asm
+++ main.asm
@@ -2,129 +2,93 @@
 .stack 100h
 .data
 
-MaxItemCount equ 40
+MAX_ITEMS equ 40
 
 ItemList dw 00, 01, 02, 03, 04, 05, 06, 07, 08, 09
             db "M.board   ",  "Memory    ", "CPU       ", "GPU       ", "Power Unit", "Comp. case", "Fans      ", "HDD       ", "SSD       ", "Monitor   "
-            dw 2, 9, 1, 2, 3, 8, 6, 3, 8, 10, 12, 13, 6, 17, 4, 19, 9, 2, 7, 5, 2, 1, 0, 0, 1, 1, 2, 1, 2, 0, '$'
+            dw 10, 1, 10, 2, 10, 3, 10, 4, 10, 5, 1500, 800, 2000, 1700, 800, 600, 400, 1200, 1500, 1300, 2, 1, 0, 0, 1, 1, 2, 1, 2, 0, '$'
 
 ItemAmount dw 2
-ItemName equ 20
-ItemNumber equ 2
-ItemPrice equ 2
-ItemInfo dw 2, 1, 0, 2, 1, 0, 40, 10, 19, 13, '$' 
-ItemCost dw 5, 8, 4, 2, 4, 9, 8, 1, 8, 10, '$'
-TotalSold dw 0
+ItemNameLength equ 20
+ItemID equ 2
+ItemValue equ 2
+ItemData dw 2, 1, 0, 2, 1, 0, 40, 10, 19, 13, '$' 
+ItemPriceList dw 5, 8, 4, 2, 4, 9, 8, 1, 8, 10, '$'
+TotalSales dw 0
 
-BlankCharacter db '                           ','$'
+BlankSpace db '                           ','$'
 
-NL equ 13           
-NLF equ 10           
-TS equ 9           
-BS equ 32        
+MainMenu db 10,10,10, "------------------------", 10, "--- Inventory System ---", 10, "------------------------", 10,10, "1. Display Items", 10, "2. Add Item", 10, "3. Sell Item", 10, "0. Exit Program", 10,10, "------------------------", '$'
 
-MainMenuTopHorizontal db NL, '   | Item INVENTORY SYSTEM |       ', NL, NLF, '=====================================', NL, NLF, NLF, '1. Display Item', NL, NLF, '2. Add Item', NL, NLF, '3. Sell Item', NL, NLF, '4. Generate Item Report', NL, NLF, '0. Go Out of Program', NL, NLF, '=====================================', NL, NLF, '$'
-InputMessageError db NL, NLF, 'Incorrect selection. Please choose only number.', NL, NLF, '$'
+InputError db 10, 'Invalid option selected.', 10, '$'
 
-ItemsTopHorizontal db NL, NLF, '              | Item DISPLAY |          ', NL, NLF, '==============================================', NL, NLF, 'Number', TS, 'Name', TS, TS, 'Price', TS, 'Quantity', NL, NLF, '$'
-ResupplyNeedMsg db '==============================================', NL, NLF, ' Items in low quantity, please ReItem item  ', NL, NLF, '==============================================', NL, NLF, '1. Go to Main Page', NL, NLF, '0. Go Out of Program', NL, NLF, 'Please make your selection: $'
+ItemHeader db 10, '==============| INVENTORY |==============', 10, '==============================================', 10, 'ID', 9, 'Name', 9, 9, 'Price', 9, 'Quantity', 10, 10, '$'
+RestockMessage db '==============================================', 10, 10, ' Items are low in stock, please restock.', 10, 10, '==============================================', 10, 10, '1. Main Menu', 10, 10, '0. Exit Program', 10, 10, 'Please select an option: $'
 
+ItemToRestock dw ?
+ItemRestockID dw ?
 
-ItemNumberSupply dw ?
-ItemNumberSupplyID dw ?
+RestockHeader db '==============================================', 10, 10, 9, 9, 32, 32, 'ADD ITEMS', 10, 10, '==============================================', 10, 10, 'Enter item ID: $'
+RestockPrompt db 10, 10, 10, 10, 'Enter quantity to restock from 1 to 9: $'
+RestockSuccess db 10, 10, 10, 10, ' Item restocked successfully.', 10, '$'
 
+SellHeader db '==============================================', 10, 10, 9, 9, 32, 32, 'SELL ITEM', 10, 10, '==============================================', 10, 10, 'Enter item ID: $'
+SellPrompt db 10, 10, 10, 10, 'Enter quantity to sell from 1 to 9: $'
+SellSuccess db 10, 10, 10, 10, ' Item sold successfully.', 10, '$'
+SellFailure db 10, 10, 10, 10, ' Insufficient quantity to sell.', 10, '$'
 
-
-ResupplyTopHorizontal db '==============================================', NL, NLF, TS, TS, BS, BS, 'ADD ITEMS', NL, NLF, '==============================================', NL, NLF, 'Enter item Number : $'
-ResupplyPromt db NL, NLF, 'What amount to replenish (1-9):  $'
-ResupplyMsgSuccess db NL, NLF, '      Item replenished Done!', NL, NLF, '$'
-
-
-
-
-
-SellItemTopHorizontal db '==============================================', NL, NLF, TS, TS, BS, BS, 'SELL Item', NL, NLF, '==============================================', NL, NLF, 'Enter item number : $'
-SellItemPromt db NL, NLF, 'Enter the quantity to sell (1-9): $'
-SellItemMsgSuccess db NL, NLF, '           Item sold Done!', NL, NLF, '$'
-SellItemMsgFailure db NL, NLF, ' Failed to sell Item. Insufficient quantity!', NL, NLF, '$'
-
-
-
-
-ReportTopHorizontal db NL, NLF, '        | Item SUMMARY REPORT SYSTEM |', NL, NLF, '==============================================', NL, NLF, 'Number', TS, 'Item', TS, TS, 'Quantity Sold', TS, 'Totally', NL, NLF, '$'
-ReportTitleHorizontal db '==============================================', NL, NLF, '         DAILY SALES SUMMARY REPORT', NL, NLF, '==============================================', NL, NLF, '1. Go to Main Page', NL, NLF, '0. Go Out of Program', NL, NLF , NL, NLF, 'Please make your selection: $'
-
-
-
-
-UserInput db NL, NLF, 'Please make your selection:  $'
-choose_off db NL, NLF, ' ----  Good bye, Item inventory system most welcome   ----','$'
-
-
-
-
+UserInputPrompt db 10, 10, 10, 10, 'Please select an option: $'
+GoodbyeMessage db 10, 10, 10, 10, '=======| Thank you for using the inventory system |=======','$'
 
 .code
-main proc
+main PROC
   mov ax, @data 
   mov ds, ax 
   
-  call ShowMain
+  call DisplayMainMenu
   
   mov ah, 01h 
   int 21h
 
   cmp al, '1'
-  je showItemMenu
+  je ShowItems
   
   cmp al, '2'
-  je ReplenishItem_menu
+  je AddItemsMenu
   
   cmp al, '3'
-  je SellItemMenu
-  
-  cmp al, '4'
-  je GenerateReportMenu
+  je SellItemsMenu
   
   cmp al, '0'
-  je EndMenu
+  je ExitProgram
 
   jmp main
 
-
-
-showItemMenu:
-    call CleanPage
-    call showItem
-    call seller_navi
+ShowItems:
+    call ClearScreen
+    call DisplayItems
+    call NavigateAfterDisplay
     ret
 
-ReplenishItem_menu:
-    call CleanPage
-    call showItem
-    call ReplenishItem
+AddItemsMenu:
+    call ClearScreen
+    call DisplayItems
+    call RestockItems
     ret
 
-SellItemMenu:
-    call CleanPage
-    call showItem
-    call SellItem
+SellItemsMenu:
+    call ClearScreen
+    call DisplayItems
+    call SellItems
     ret
 
-GenerateReportMenu:
-    call GenerateReport
-    call report_navi
+ExitProgram:
+    call ClearScreen
+    call Goodbye
     ret
 
-EndMenu:
-    call CleanPage
-    call EndSystem
-    ret
-
-
-
-seller_navi:
-    lea dx, ResupplyNeedMsg
+NavigateAfterDisplay:
+    lea dx, RestockMessage
     mov ah, 09h
     int 21h
 
@@ -132,7 +96,7 @@ seller_navi:
     int 21h
 
     cmp al, '0'
-    je EndMenu
+    je ExitProgram
 
     cmp al, '1'
     je main
@@ -141,157 +105,126 @@ seller_navi:
 
     ret
 
-report_navi:
-    lea dx, ReportTitleHorizontal
-    mov ah, 09h
-    int 21h
-
-    mov ah, 01h 
-    int 21h
-
-    cmp al, '0'
-    je EndMenu
-
-    cmp al, '1'
-    je main
-
-    jmp main
-
-    ret
-
-
-ShowInteger:
+PrintInteger:
     push bx
     mov bx, 10
     xor cx, cx
 
-    BecomeLoopInner:
-        xor dx, dx
-        div bx
-        add dl, '0'
-        push dx
-        inc cx
-        cmp ax, 0
-        jne BecomeLoopInner
+InnerLoop:
+    xor dx, dx
+    div bx
+    add dl, '0'
+    push dx
+    inc cx
+    cmp ax, 0
+    jne InnerLoop
 
-    ShowSecondLoop:
-        pop dx
-        mov ah, 02
-        int 21h
-        dec cx
-        cmp cx, 0
-        jne ShowSecondLoop
-        pop bx
-        ret
-
-
-intiger5:
-    mov bx, ax
-    cmp bx, 5
-    jle Marking
+PrintLoop:
+    pop dx
+    mov ah, 02
+    int 21h
+    dec cx
+    cmp cx, 0
+    jne PrintLoop
+    pop bx
     ret
 
-showText:
+CheckValue5:
+    mov bx, ax
+    cmp bx, 5
+    jle MarkValue
+    ret
+
+PrintString:
     push ax 
     push bx
     push cx
     mov bx, dx 
     mov cx, 10 
 
-ShowLoop:
+StringLoop:
     mov dl, [bx] 
     int 21h 
     inc bx 
-    loop ShowLoop 
+    loop StringLoop 
 
-finish:
+Done:
     pop cx 
     pop bx
     pop ax
     ret
 
-
-
-
-Marking:
+MarkValue:
     push ax 
     push bx
     push cx
     mov bx, dx 
     mov cx, 10 
 
-ShowLoop3:
+MarkLoop:
     mov dl, [bx] 
     mov ah, 09h
     mov al, dl 
     mov bl, 0Eh 
     int 10h
     inc bx 
-    loop ShowLoop3 
+    loop MarkLoop 
 
-finish3:
+MarkDone:
     pop cx 
     pop bx
     pop ax
     ret
 
-
-
-ShowMain:
-    call CleanPage
-    lea dx, MainMenuTopHorizontal
+DisplayMainMenu:
+    call ClearScreen
+    lea dx, MainMenu
     mov ah, 09h
     int 21h
     
-    lea dx, UserInput
+    lea dx, UserInputPrompt
     mov ah, 09h
     int 21h
     ret
 
-
-
-
-showItem:
-    mov dx, offset ItemsTopHorizontal
+DisplayItems:
+    mov dx, offset ItemHeader
     mov ah, 09
     int 21h
     
     mov bp, 0
     lea si, ItemList
 
-LoopBegin:
+ItemLoop:
     mov ax, [si]
     cmp ax, 10
-    ja done
-    call ShowInteger
-    call ShowBS
+    ja ItemsDone
+    call PrintInteger
+    call PrintTab
 
     mov dx, offset ItemList + 20
     add dx, bp
-    call showText
-    call ShowBS
+    call PrintString
+    call PrintTab
 
     mov ax, [si + 140]
-    call ShowInteger
-    call ShowBS
+    call PrintInteger
+    call PrintTab
 
     mov ax, [si + 120]
-    call intiger5
+    call CheckValue5
 
     mov ax, [si + 120]
-    call ShowInteger
-    call ShowNext
+    call PrintInteger
+    call PrintNewLine
     add bp, 10
     add si, 2
-    jmp LoopBegin
-done:
+    jmp ItemLoop
+ItemsDone:
     ret
 
-
-
-
-ReplenishItem:
-    lea dx, ResupplyTopHorizontal
+RestockItems:
+    lea dx, RestockHeader
     mov ah, 09h
     int 21h 
     mov ah, 01
@@ -300,8 +233,8 @@ ReplenishItem:
     add al, al
     sub ax, 136
 
-    mov ItemNumberSupply, ax 
-    lea dx, ResupplyPromt
+    mov ItemToRestock, ax 
+    lea dx, RestockPrompt
     mov ah, 09h 
     int 21h
 
@@ -311,25 +244,23 @@ ReplenishItem:
     sub ax, 256
     mov cx, ax
     lea si, ItemList
-    add si, ItemNumberSupply
+    add si, ItemToRestock
     add cx, [si]
     mov word ptr [si], cx 
     
-    call CleanPage
-    call ShowNext
-    call ShowBegin
-    lea dx, ResupplyMsgSuccess
+    call ClearScreen
+    call PrintNewLine
+    call PrintBlank
+    lea dx, RestockSuccess
     mov ah, 09h 
     int 21h 
-    call ShowBegin
-    call showItem
-    call seller_navi
+    call PrintBlank
+    call DisplayItems
+    call NavigateAfterDisplay
     ret
 
-
-
-SellItem:
-    lea dx, SellItemTopHorizontal
+SellItems:
+    lea dx, SellHeader
     mov ah, 09h
     int 21h 
 
@@ -339,9 +270,9 @@ SellItem:
     sub al, 30h
     add al, al 
     sub ax, 136 
-    mov ItemNumberSupply, ax 
+    mov ItemToRestock, ax 
 
-    lea dx, SellItemPromt
+    lea dx, SellPrompt
     mov ah, 09h 
     int 21h
 
@@ -352,48 +283,46 @@ SellItem:
     mov cx, ax
 
     lea si, ItemList
-    add si, ItemNumberSupply
+    add si, ItemToRestock
     mov bx, [si] 
     sub bx, cx
     cmp bx, 0
-    js NewAmout
+    js InsufficientQuantity
 
     mov word ptr [si], bx
-    jmp SoldAmount
+    jmp ItemSold
 
-NewAmout: 
+InsufficientQuantity: 
     mov bx, [si]
     mov word ptr [si], bx
-    call CleanPage
-    call ShowNext
-    call ShowBegin
-    lea dx, SellItemMsgFailure
+    call ClearScreen
+    call PrintNewLine
+    call PrintBlank
+    lea dx, SellFailure
     mov ah, 09h 
     int 21h 
-    call ShowBegin
-    call ShowNext
-    call showItem
-    call seller_navi
+    call PrintBlank
+    call PrintNewLine
+    call DisplayItems
+    call NavigateAfterDisplay
     ret 
 
-SoldAmount:
-
-    call sold_finish
-    call CleanPage
-    call ShowNext
-    call ShowBegin
-    lea dx, SellItemMsgSuccess
+ItemSold:
+    call UpdateTotalSales
+    call ClearScreen
+    call PrintNewLine
+    call PrintBlank
+    lea dx, SellSuccess
     mov ah, 09h
     int 21h
 
-    call ShowBegin
-    call ShowNext
-    call showItem
-    call seller_navi
+    call PrintBlank
+    call PrintNewLine
+    call DisplayItems
+    call NavigateAfterDisplay
     ret
     
-
-sold_finish: 
+UpdateTotalSales: 
     mov ax, ItemAmount 
     sub ax, 120 
     mov ItemAmount, ax
@@ -403,66 +332,15 @@ sold_finish:
     add cx, ax 
     mov word ptr [si], cx
     ret
-  ret
 
-
-GenerateReport:
-    call CleanPage
-    mov dx, offset ReportTopHorizontal
-    mov ah, 09
-    int 21h
-    
-    mov bp, 0
-    lea si, ItemList
-    mov bx, offset ItemInfo
-    mov di, offset ItemCost 
-
-
-    LoopBegin2:
-        mov ax, [si] 
-        cmp ax, 10 
-        ja done2 
-        call ShowInteger 
-        call ShowBS
-
-        mov dx, offset ItemList + 20 
-        add dx, bp 
-        call showText 
-        call ShowBS
-
-
-        mov ax, [bx] 
-        call ShowInteger 
-        call ShowExtraBS
-        
-
-        mov cx, [bx]
-        mov ax, [di]
-        mul cx
-        call ShowInteger
-        call ShowNext
-
-        add bp, 10
-        add si, 2 
-        add bx, 2
-        add di, 2
-        jmp LoopBegin2 
-        
-    done2:
-    ret
-
-
-EndSystem:
-  lea dx, choose_off
+Goodbye:
+  lea dx, GoodbyeMessage
   mov ah, 09h
   int 21h  
   mov ah, 4ch
   int 21h
 
-
-
-
-CleanPage:
+ClearScreen:
     mov ah, 06h
     mov al, 0
     mov bh, 07h
@@ -471,37 +349,23 @@ CleanPage:
     int 10h
     ret
 
-ShowLine:
-    lea dx, BlankCharacter
-    mov ah, 09h
-    int 21h
-    ret
-
-ShowBS:
+PrintTab:
     mov dl, 9
     mov ah, 02
     int 21h
     ret
 
-ShowExtraBS:
-    mov dl, 9
-    mov ah, 02
-    int 21h
-    int 21h
-    ret
-
-ShowNext:
+PrintNewLine:
     mov dl, 0ah
     mov ah, 02
     int 21h
     ret
 
-ShowBegin:
-    lea dx, BlankCharacter
+PrintBlank:
+    lea dx, BlankSpace
     mov ah, 09h 
     int 21h 
     ret
-
 
 main endp
 end main
